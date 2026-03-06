9c2cc362aad9fa1f5cf94b1f546221e6d26fd605
Boscoli <ickboscoli@gmail.com>
2026-03-06 03:03:12 -0300

    Corrigindo arquivos

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 import boto3
 
-dynamodb = boto3.resource("dynamodb")
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name="sa-east-1"
+)
 
 def get_table():
     return dynamodb.Table("clima_dados")
